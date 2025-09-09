70d23b50fe2936f7077a9900e3c8d5cc49e3b711
Pablo Gómez <149898543+pablohf7@users.noreply.github.com>
2025-09-08 20:26:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -120,4 +120,6 @@ def actualizar_dashboard(equipo_seleccionado, categoria_seleccionada):
 
 # ================== Run ================== #
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run_server(debug=False, host="0.0.0.0", port=port)
